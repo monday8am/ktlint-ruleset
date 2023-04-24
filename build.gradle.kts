4f8cf977033d7e7bbf63f98b06d513dfd6293c9d
Anton <hello@monday8am.com>
2023-04-24 21:52:16 +0200

    Add maven publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
 plugins {
     kotlin("jvm") version "1.8.0"
+    `java-library`
+    `maven-publish`
 }
 
-group = "de.komoot.ruleset.ktlint.android"
-version = "1.0"
+group = "com.github.monday8am"
+version = "v0.0.2"
 val kotlinVersion = "0.49.0"
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
 }
 
 dependencies {
@@ -15,6 +18,39 @@ dependencies {
     implementation("com.pinterest.ktlint:ktlint-cli-ruleset-core:${kotlinVersion}")
     testImplementation("com.pinterest.ktlint:ktlint-test:${kotlinVersion}")
     testImplementation(kotlin("test"))
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    dependsOn(tasks.classes)
+    archiveClassifier.set("sources")
+    from(sourceSets.main.map { it.allSource })
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    dependsOn(tasks.javadoc)
+    archiveClassifier.set("javadoc")
+    from(tasks.javadoc.map { it.destinationDir!! })
+}
+
+artifacts {
+    archives(sourcesJar)
+    archives(javadocJar)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            pom {
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+            }
+        }
+    }
 }
 
 tasks.test {
